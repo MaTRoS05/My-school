fe14645e1115daa5afc788add53b55cb600f27c8
MaTRoS05 <123573586+MaTRoS05@users.noreply.github.com>
2025-02-19 20:33:30 +0000

    main

--- get_days.py
+++ get_days.py
@@ -2,11 +2,13 @@ from datetime import datetime, date
 
 def get_days_from_today(date_str):
 
-    date_format = '%Y-%m-%d'
-    given_date = datetime.strptime(date_str, date_format).date()
-    today = date.today()
+    date_format = "%Y-%m-%d"
+    try:
+        given_date = datetime.strptime(date_str, date_format).date()
+        today = date.today()
+        return (given_date - today).days
+    except ValueError:
+        print("Invalid date format. Please use 'YYYY-MM-DD'.")
+        return None 
     
-    days_difference = (given_date - today).days
-    
-    return days_difference
-print (get_days_from_today("2025-10-09"))
+print (get_days_from_today("2025-05-10"))
--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -1,13 +1,15 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):
-    random_numbers = random.sample(range(min, max + 1), quantity)
-    return sorted(random_numbers)
+def get_numbers_ticket(min_value, max_value, quantity):
+    if not (1 <= min_value <= max_value <= 1000) or quantity > (max_value - min_value + 1):
+        print("Invalid input: quantity cannot be greater than the available range.")
+        return []
+
+    return sorted(random.sample(range(min_value, max_value + 1), quantity))
 
 min_value = 1
 max_value = 1000
-quantity_value = 10
+quantity_value = 10 
 
 ticket_numbers = get_numbers_ticket(min_value, max_value, quantity_value)
-
 print("Generated ticket numbers:", ticket_numbers)
